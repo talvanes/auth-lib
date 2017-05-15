429c23e6e4d27141118b8af5d84b870e09af21b3
talvanes <tsousa@girolando.com.br>
2017-05-15 14:48:42 -0300

    Begin AuthContext implementation

--- tests/bootstrap/AuthContext.php
+++ tests/bootstrap/AuthContext.php
@@ -5,28 +5,85 @@ use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Hook\Scope\AfterScenarioScope;
+
 /**
  * Defines application features from the specific context.
  */
 class AuthContext implements Context
 {
+    protected $db;
+    protected $auth;
+
     /**
-     * Initializes context.
+     * * Initializes context.
      *
      * Every scenario gets its own context instance.
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
+     *
+     * @public
+     * @param string $dsn                 DB DSN
+     * @param string [$username = 'root'] DB username (default: root)
+     * @param string [$password = '']     DB password (default: '')
      */
-    public function __construct()
+    public function __construct($dsn, $username = 'root', $password = '')
     {
+        // set up database connection
+        $this->db = new PDO($dsn, $username, $password, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]);
     }
+
+    /**
+     * Destructor.
+     *
+     * As it ends the context, it shuts down database connection
+     * @public
+     */
+    public function __destruct()
+    {
+        // kill database connection
+        $this->db = null;
+    }
+
+
+    /**
+     * @BeforeScenario
+     */
+    public function beginTrans(BeforeScenarioScope $scope)
+    {
+        $this->db->beginTransaction();
+    }
+
+    /**
+     * @AfterScenario
+     */
+    public function rollbackTrans(AfterScenarioScope $scope)
+    {
+        $this->db->rollBack();
+    }
+
 
     /**
      * @Given there are users:
      */
-    public function thereAreUsers(TableNode $table)
+    public function thereAreUsers(TableNode $usersTable)
     {
-        throw new PendingException();
+        // prepare PDO statement for insert query
+        $insertStmt = $this->db->prepare("INSERT INTO (username, password, name) VALUES (:username, :password, :name)");
+
+        // insert records into database
+        foreach ($usersTable as $userData) {
+            $insertStmt->execute([
+                'username' => $userData['username'],
+                'password' => password_hash($userData['password'], PASSWORD_BCRYPT),
+                'name' => $userData['name'],
+            ]);
+        }
     }
 
     /**
